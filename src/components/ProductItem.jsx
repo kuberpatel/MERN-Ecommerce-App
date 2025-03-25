1add63e36fd7d04b6daa5c83c09893e4d22f9534
Kuber Patel <kuberpatel@Kubers-MacBook-Air.local>
2025-03-25 23:11:38 +0530

    Updated ProductItem and Product pages, added RelatedProducts component

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import {Link} from 'react-router-dom'
 
-const ProductItem = ({id,image,name,price}) => {
+ const ProductItem = ({id,image,name,price}) => {
     const {currency} = useContext(ShopContext);
   return (
     <Link className='text-gray-700 cursor-pointer' to={`/products/${id}`}>
